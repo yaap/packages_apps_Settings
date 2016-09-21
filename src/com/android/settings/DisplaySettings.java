b18b8eaa1b1e7f8daf4607636541da0ec81a0fdb
ezio84 <brabus84@gmail.com>
2016-09-21 13:06:12 +0200

    Move custom DP settings under display submenu
    Change-Id: I303ca5a00e555e5cf3bd3b488aba0314ec7bba1b

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.os.Bundle;
 
 import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.development.DefaultLaunchPreferenceController;
 import com.android.settings.development.OverlayCategoryPreferenceController;
 import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
@@ -91,6 +92,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
+        controllers.add(new DefaultLaunchPreferenceController(context, "density"));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.accent_color"));
         controllers.add(new OverlayCategoryPreferenceController(context,
