9f029f1a26d91cf1f2ae19e632bec4b55aa6c14d
Ido Ben-Hur <idoybh2@gmail.com>
2022-02-23 22:14:49 +0200

    Settings: Add navbar layout inversion tuning [2/2]
    Change-Id: I08c43664da6e036f36e86d00ecd31573ddb1e667

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -111,6 +111,7 @@ import com.android.settings.gestures.AssistGestureSettings;
 import com.android.settings.gestures.ButtonNavigationSettingsFragment;
 import com.android.settings.gestures.DoubleTapScreenSettings;
 import com.android.settings.gestures.DoubleTwistGestureSettings;
+import com.android.settings.gestures.ButtonNavigationSettingsFragment;
 import com.android.settings.gestures.GestureNavigationSettingsFragment;
 import com.android.settings.gestures.OneHandedSettings;
 import com.android.settings.gestures.PowerMenuSettings;
@@ -353,6 +354,7 @@ public class SettingsGateway {
             PowerMenuSettings.class.getName(),
             DarkModeSettingsFragment.class.getName(),
             BugReportHandlerPicker.class.getName(),
+            ButtonNavigationSettingsFragment.class.getName(),
             GestureNavigationSettingsFragment.class.getName(),
             ButtonNavigationSettingsFragment.class.getName(),
             InteractAcrossProfilesSettings.class.getName(),
