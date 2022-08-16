cddd322bd6a5f1b00bb4d86db77f3978fd3feb77
SamarV-121 <samarvispute121@pm.me>
2022-08-16 09:58:10 +0200

    Revert "Recreate fingerprint setup if degree 0 to 180"
    This reverts commit 23b95b41479ac6463b0e5789f6b60275c12c852e.
    
    Change-Id: I6b44bf2be2cfecff3ec276af2e615c2f30460b43

--- src/com/android/settings/biometrics/fingerprint/FingerprintEnrollFindSensor.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintEnrollFindSensor.java
@@ -115,11 +115,6 @@ public class FingerprintEnrollFindSensor extends BiometricEnrollBase implements
                     lottieAnimationView.setVisibility(View.GONE);
                     lottieAnimationViewPortrait.setVisibility(View.VISIBLE);
                     break;
-                case Surface.ROTATION_180:
-                    lottieAnimationView.setVisibility(View.VISIBLE);
-                    lottieAnimationView.setRotation(180);
-                    lottieAnimationViewPortrait.setVisibility(View.GONE);
-                    break;
                 case Surface.ROTATION_270:
                     lottieAnimationView.setVisibility(View.GONE);
                     lottieAnimationViewPortrait.setVisibility(View.VISIBLE);
@@ -364,7 +359,10 @@ public class FingerprintEnrollFindSensor extends BiometricEnrollBase implements
             @Override
             public void onOrientationChanged(int orientation) {
                 final int currentRotation = getDisplay().getRotation();
-                if ((currentRotation + 2) % 4 == mPreviousRotation) {
+                if ((mPreviousRotation == Surface.ROTATION_90
+                        && currentRotation == Surface.ROTATION_270) || (
+                        mPreviousRotation == Surface.ROTATION_270
+                                && currentRotation == Surface.ROTATION_90)) {
                     mPreviousRotation = currentRotation;
                     recreate();
                 }
