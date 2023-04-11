b5d2854346181ffde93a951e3551c853202389fb
Ido Ben-Hur <idoybh2@gmail.com>
2023-04-11 17:06:18 +0300

    Settings: Restore dt2w & pickup gestures fragments
    But convert em to a com.android.settingslib.PrimarySwitchPreference
    As prep for what comes next!

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -109,9 +109,10 @@ import com.android.settings.fuelgauge.batterysaver.BatterySaverSettings;
 import com.android.settings.fuelgauge.batteryusage.PowerUsageSummary;
 import com.android.settings.gaming.GamingModeFragment;
 import com.android.settings.gestures.ButtonNavigationSettingsFragment;
-import com.android.settings.gestures.ButtonNavigationSettingsFragment;
+import com.android.settings.gestures.DoubleTapScreenSettings;
 import com.android.settings.gestures.GestureNavigationSettingsFragment;
 import com.android.settings.gestures.OneHandedSettings;
+import com.android.settings.gestures.PickupGestureSettings;
 import com.android.settings.gestures.PowerMenuSettings;
 import com.android.settings.gestures.SystemNavigationGestureSettings;
 import com.android.settings.inputmethod.AvailableVirtualKeyboardFragment;
@@ -266,6 +267,8 @@ public class SettingsGateway {
             FingerprintSettings.FingerprintSettingsFragment.class.getName(),
             CombinedBiometricSettings.class.getName(),
             CombinedBiometricProfileSettings.class.getName(),
+            DoubleTapScreenSettings.class.getName(),
+            PickupGestureSettings.class.getName(),
             SystemNavigationGestureSettings.class.getName(),
             DataUsageSummary.class.getName(),
             DreamSettings.class.getName(),
