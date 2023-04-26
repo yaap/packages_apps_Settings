ba2f5e44e45a4f24803390ccc4d5b0c6f1cf62d2
Ido Ben-Hur <idoybh2@gmail.com>
2023-04-26 12:14:05 +0300

    Settings: Allow ambient instead of waking in tap as well [2/2]
    This also brings back the single tap page and converts the whole thing to a com.android.settingslib.PrimarySwitchPreference
    Follows our convention in the gesture page

--- src/com/android/settings/gestures/TapScreenGesturePreferenceController.java
+++ src/com/android/settings/gestures/TapScreenGesturePreferenceController.java
@@ -19,29 +19,43 @@ package com.android.settings.gestures;
 import static android.provider.Settings.Secure.DOZE_TAP_SCREEN_GESTURE;
 
 import android.annotation.UserIdInt;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.ContentObserver;
 import android.hardware.display.AmbientDisplayConfiguration;
+import android.net.Uri;
+import android.os.Handler;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.Settings;
 
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
 import com.android.settings.R;
 import com.android.settings.core.TogglePreferenceController;
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnStart;
+import com.android.settingslib.core.lifecycle.events.OnStop;
+import com.android.settingslib.PrimarySwitchPreference;
 
-public class TapScreenGesturePreferenceController extends TogglePreferenceController {
+public class TapScreenGesturePreferenceController extends TogglePreferenceController
+        implements LifecycleObserver, OnStart, OnStop {
+
+    private static final String SECURE_KEY = DOZE_TAP_SCREEN_GESTURE;
+    private static final String AMBIENT_SECURE_KEY = "doze_tap_gesture_ambient";
 
     private AmbientDisplayConfiguration mAmbientConfig;
     @UserIdInt
     private final int mUserId;
 
+    private PrimarySwitchPreference mPreference;
+    private SettingObserver mSettingObserver;
+
     public TapScreenGesturePreferenceController(Context context, String key) {
         super(context, key);
         mUserId = UserHandle.myUserId();
-    }
-
-    public TapScreenGesturePreferenceController setConfig(AmbientDisplayConfiguration config) {
-        mAmbientConfig = config;
-        return this;
+        mAmbientConfig = new AmbientDisplayConfiguration(context);
     }
 
     @Override
@@ -52,6 +66,31 @@ public class TapScreenGesturePreferenceController extends TogglePreferenceContro
         }
 
         return AVAILABLE;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+        mSettingObserver = new SettingObserver(mPreference);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        super.updateState(preference);
+        final ContentResolver resolver = mContext.getContentResolver();
+        final boolean enabled =
+                Settings.Secure.getInt(resolver, SECURE_KEY, 1) == 1;
+        String summary;
+        if (enabled) {
+            summary = mContext.getString(R.string.gesture_setting_on) + " ("
+                    + (Settings.Secure.getInt(resolver, AMBIENT_SECURE_KEY, 0) == 1
+                    ? mContext.getString(R.string.gesture_wake_ambient)
+                    : mContext.getString(R.string.gesture_wake)) + ")";
+        } else {
+            summary = mContext.getString(R.string.gesture_setting_off);
+        }
+        preference.setSummary(summary);
     }
 
     @Override
@@ -72,7 +111,7 @@ public class TapScreenGesturePreferenceController extends TogglePreferenceContro
     @Override
     public boolean setChecked(boolean isChecked) {
         boolean success = Settings.Secure.putInt(mContext.getContentResolver(),
-                DOZE_TAP_SCREEN_GESTURE, isChecked ? 1 : 0);
+                SECURE_KEY, isChecked ? 1 : 0);
         SystemProperties.set("persist.sys.tap_gesture", isChecked ? "1" : "0");
         return success;
     }
@@ -87,5 +126,49 @@ public class TapScreenGesturePreferenceController extends TogglePreferenceContro
     @Override
     public int getSliceHighlightMenuRes() {
         return R.string.menu_key_yasp;
+    }
+
+    @Override
+    public void onStart() {
+        if (mSettingObserver != null) {
+            mSettingObserver.register(mContext.getContentResolver());
+            mSettingObserver.onChange(false, null);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (mSettingObserver != null) {
+            mSettingObserver.unregister(mContext.getContentResolver());
+        }
+    }
+
+    private class SettingObserver extends ContentObserver {
+        private final Uri mUri = Settings.Secure.getUriFor(SECURE_KEY);
+        private final Uri mAmbientUri = Settings.Secure.getUriFor(AMBIENT_SECURE_KEY);
+
+        private final Preference mPreference;
+
+        SettingObserver(Preference preference) {
+            super(Handler.getMain());
+            mPreference = preference;
+        }
+
+        public void register(ContentResolver cr) {
+            cr.registerContentObserver(mUri, false, this);
+            cr.registerContentObserver(mAmbientUri, false, this);
+        }
+
+        public void unregister(ContentResolver cr) {
+            cr.unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+            if (uri == null || mUri.equals(uri) || mAmbientUri.equals(uri)) {
+                updateState(mPreference);
+            }
+        }
     }
 }
