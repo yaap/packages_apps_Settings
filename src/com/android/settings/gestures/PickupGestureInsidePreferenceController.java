31fae2ce144f08c870394b3122a8523f29591302
Ido Ben-Hur <idoybh2@gmail.com>
2023-07-02 19:01:00 +0300

    Settings: gestures: Add availability check for in-page controllers
    These should be unavailable when the main pref is

--- src/com/android/settings/gestures/PickupGestureInsidePreferenceController.java
+++ src/com/android/settings/gestures/PickupGestureInsidePreferenceController.java
@@ -17,6 +17,7 @@
 package com.android.settings.gestures;
 
 import android.content.Context;
+import android.hardware.display.AmbientDisplayConfiguration;
 import android.provider.Settings;
 import android.widget.Switch;
 
@@ -40,6 +41,7 @@ public class PickupGestureInsidePreferenceController extends AbstractPreferenceC
 
     private final boolean mDefault;
     private final Context mContext;
+    private AmbientDisplayConfiguration mAmbientConfig;
     private MainSwitchPreference mSwitch;
     private SecureSettingSwitchPreference mAmbientPref;
     private SecureSettingSwitchPreference mAODPref;
@@ -91,7 +93,7 @@ public class PickupGestureInsidePreferenceController extends AbstractPreferenceC
 
     @Override
     public boolean isAvailable() {
-        return true;
+        return getAmbientConfig().dozePickupSensorAvailable();
     }
 
     @Override
@@ -104,5 +106,13 @@ public class PickupGestureInsidePreferenceController extends AbstractPreferenceC
     private void updateEnablement(boolean enabled) {
         if (mAmbientPref != null) mAmbientPref.setEnabled(enabled);
         if (mAODPref != null) mAODPref.setEnabled(enabled);
+    }
+
+    private AmbientDisplayConfiguration getAmbientConfig() {
+        if (mAmbientConfig == null) {
+            mAmbientConfig = new AmbientDisplayConfiguration(mContext);
+        }
+
+        return mAmbientConfig;
     }
 }
