98fbb69b4acce75321b185e34ff2005c0a7f9076
John Galt <johngaltfirstrun@gmail.com>
2024-02-26 11:15:44 -0500

    Revert "Settings: Make all user apps cloneable"
    This reverts commit baa2e5a88386c4187008de7f6e5cc39a73e051ba.

--- src/com/android/settings/applications/AppStateClonedAppsBridge.java
+++ src/com/android/settings/applications/AppStateClonedAppsBridge.java
@@ -24,7 +24,6 @@ import static com.android.settingslib.applications.ApplicationsState.AppFilter;
 import android.content.Context;
 import android.util.Log;
 
-import com.android.settings.custom.utils.AppUtils;
 import com.android.settings.Utils;
 import com.android.settingslib.applications.ApplicationsState;
 
@@ -43,13 +42,13 @@ public class AppStateClonedAppsBridge extends AppStateBaseBridge{
     private final List<String> mAllowedApps;
     private List<String> mCloneProfileApps = new ArrayList<>();
     private int mCloneUserId;
-    private AppUtils appUtils = new AppUtils();
 
     public AppStateClonedAppsBridge(Context context, ApplicationsState appState,
             Callback callback) {
         super(appState, callback);
         mContext = context;
-        mAllowedApps = appUtils.getCloneableAppListStr(mContext);
+        mAllowedApps = Arrays.asList(mContext.getResources()
+                .getStringArray(com.android.internal.R.array.cloneable_apps));
     }
 
     @Override
