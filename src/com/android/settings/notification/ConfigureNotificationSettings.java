3da46987d2c971ffec6b0614a16cc46bb285debc
Ido Ben-Hur <idoybh2@gmail.com>
2024-04-09 21:10:12 +0300

    Settings: Move YASP notification settings here

--- src/com/android/settings/notification/ConfigureNotificationSettings.java
+++ src/com/android/settings/notification/ConfigureNotificationSettings.java
@@ -23,16 +23,21 @@ import android.app.Activity;
 import android.app.Application;
 import android.app.settings.SettingsEnums;
 import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.provider.Settings;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 
+import com.android.internal.util.yaap.YaapUtils;
 import com.android.settings.R;
 import com.android.settings.RingtonePreference;
 import com.android.settings.core.OnActivityResultListener;
@@ -41,12 +46,17 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.yasp.settings.preferences.CustomSeekBarPreference;
+import com.yasp.settings.preferences.SystemSettingListPreference;
+import com.yasp.settings.preferences.SystemSettingSwitchPreference;
+import com.yasp.settings.Utils;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @SearchIndexable
 public class ConfigureNotificationSettings extends DashboardFragment implements
-        OnActivityResultListener {
+        OnActivityResultListener, OnPreferenceChangeListener {
     private static final String TAG = "ConfigNotiSettings";
 
     @VisibleForTesting
@@ -58,9 +68,22 @@ public class ConfigureNotificationSettings extends DashboardFragment implements
     private static final String SELECTED_PREFERENCE_KEY = "selected_preference";
     private static final String KEY_ADVANCED_CATEGORY = "configure_notifications_advanced";
 
+    // YASP Import
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+    private static final String FLASH_ON_CALL_OPTIONS = "on_call_flashlight_category";
+    private static final String PREF_FLASH_ON_CALL = "flashlight_on_call";
+    private static final String PREF_FLASH_ON_CALL_DND = "flashlight_on_call_ignore_dnd";
+    private static final String PREF_FLASH_ON_CALL_RATE = "flashlight_on_call_rate";
+    private static final String KEY_BATT_LIGHT = "battery_light_enabled";
+
     private RingtonePreference mRequestPreference;
 
     private NotificationAssistantPreferenceController mNotificationAssistantPreferenceController;
+
+    // YASP Import
+    private SystemSettingListPreference mFlashOnCall;
+    private SystemSettingSwitchPreference mFlashOnCallIgnoreDND;
+    private CustomSeekBarPreference mFlashOnCallRate;
 
     @Override
     public int getMetricsCategory() {
@@ -81,6 +104,40 @@ public class ConfigureNotificationSettings extends DashboardFragment implements
         replaceEnterpriseStringSummary("lock_screen_work_redact",
                 WORK_PROFILE_LOCK_SCREEN_REDACT_NOTIFICATION_SUMMARY,
                 R.string.lock_screen_notifs_redact_work_summary);
+
+        // YASP Import
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!Utils.isVoiceCapable(getActivity())) {
+            prefScreen.removePreference(incallVibCategory);
+        }
+
+        if (!YaapUtils.deviceHasFlashlight(getActivity())) {
+            PreferenceCategory flashOnCallCategory = (PreferenceCategory)
+                    findPreference(FLASH_ON_CALL_OPTIONS);
+            prefScreen.removePreference(flashOnCallCategory);
+        } else {
+            mFlashOnCallRate = (CustomSeekBarPreference)
+                    findPreference(PREF_FLASH_ON_CALL_RATE);
+            int value = Settings.System.getInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL_RATE, 1);
+            mFlashOnCallRate.setValue(value);
+            mFlashOnCallRate.setOnPreferenceChangeListener(this);
+
+            mFlashOnCallIgnoreDND = (SystemSettingSwitchPreference)
+                    findPreference(PREF_FLASH_ON_CALL_DND);
+            value = Settings.System.getInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, 0);
+            mFlashOnCallIgnoreDND.setVisible(value > 1);
+            mFlashOnCallRate.setVisible(value != 0);
+
+            mFlashOnCall = (SystemSettingListPreference)
+                    findPreference(PREF_FLASH_ON_CALL);
+            mFlashOnCall.setSummary(mFlashOnCall.getEntries()[value]);
+            mFlashOnCall.setOnPreferenceChangeListener(this);
+        }
     }
 
     @Override
@@ -157,6 +214,26 @@ public class ConfigureNotificationSettings extends DashboardFragment implements
         if (mRequestPreference != null) {
             outState.putString(SELECTED_PREFERENCE_KEY, mRequestPreference.getKey());
         }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mFlashOnCall) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, value);
+            mFlashOnCall.setSummary(mFlashOnCall.getEntries()[value]);
+            mFlashOnCallIgnoreDND.setVisible(value > 1);
+            mFlashOnCallRate.setVisible(value != 0);
+            return true;
+        } else if (preference == mFlashOnCallRate) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL_RATE, value);
+            return true;
+        }
+        return false;
     }
 
     /**
